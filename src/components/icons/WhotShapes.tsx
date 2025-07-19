25f0c269d5e103da1779a57757ec9aec6cae4c7d
Joseph Omotade <sketchartworks603@gmail.com>
2025-07-19 08:00:12 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/icons/WhotShapes.tsx
+++ src/components/icons/WhotShapes.tsx
@@ -20,23 +20,23 @@ const smallShapeColors: Record<Shape, string> = {
     whot: ""
 }
 
-const Circle = (props: SVGProps<SVGSVGElement> & {small?: boolean}) => (
-    <svg viewBox="0 0 100 100" {...props}><circle cx="50" cy="50" r={props.small ? 50: 45} fill="currentColor" /></svg>
+const Circle = ({ small, ...props }: SVGProps<SVGSVGElement> & {small?: boolean}) => (
+    <svg viewBox="0 0 100 100" {...props}><circle cx="50" cy="50" r={small ? 50: 45} fill="currentColor" /></svg>
 );
 
-const Triangle = (props: SVGProps<SVGSVGElement>  & {small?: boolean}) => (
+const Triangle = ({ small, ...props }: SVGProps<SVGSVGElement>  & {small?: boolean}) => (
     <svg viewBox="0 0 100 100" {...props}><path d="M50 5 L95 95 L5 95 Z" fill="currentColor" /></svg>
 );
 
-const Cross = (props: SVGProps<SVGSVGElement>  & {small?: boolean}) => (
+const Cross = ({ small, ...props }: SVGProps<SVGSVGElement>  & {small?: boolean}) => (
     <svg viewBox="0 0 100 100" {...props}><path d="M85 5 L95 15 L60 50 L95 85 L85 95 L50 60 L15 95 L5 85 L40 50 L5 15 L15 5 L50 40 Z" fill="currentColor" /></svg>
 );
 
-const Square = (props: SVGProps<SVGSVGElement>  & {small?: boolean}) => (
+const Square = ({ small, ...props }: SVGProps<SVGSVGElement>  & {small?: boolean}) => (
     <svg viewBox="0 0 100 100" {...props}><rect x="10" y="10" width="80" height="80" fill="currentColor" /></svg>
 );
 
-const Star = (props: SVGProps<SVGSVGElement>  & {small?: boolean}) => (
+const Star = ({ small, ...props }: SVGProps<SVGSVGElement>  & {small?: boolean}) => (
     <svg viewBox="0 0 100 100" {...props}><path d="M50 5 L61.8 38.2 L98.2 38.2 L68.2 59.5 L79.5 92.7 L50 71 L20.5 92.7 L31.8 59.5 L1.8 38.2 L38.2 38.2 Z" fill="currentColor" /></svg>
 );
 
